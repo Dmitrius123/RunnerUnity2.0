3f348d706904ad751a9e9f85bc3e97e4a57d0168
Dmitrius123 <S2109011784@sd.uni-vt.bg>
2024-04-05 20:35:26 +0300

    ITS READY

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Animator anim;
+    public UnityEngine.Vector3 dir;
+    
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity;
+    [SerializeField] private int coins;
+    [SerializeField] private GameObject losePanel;
+    [SerializeField] private Text coinsText;
+
+    private bool isJumping;
+
+    private int lineToMove = 1;
+    public float lineDistance = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        controller = GetComponent<CharacterController>();
+        Time.timeScale = 1;
+        coins = PlayerPrefs.GetInt("coins");
+        coinsText.text = coins.ToString();
+    }
+
+    private void Jump()
+    {
+        dir.y = jumpForce;
+        anim.SetTrigger("isJumping");
+    }
+
+    private void Update()
+    {
+        if (SwipeController.swipeRight)
+        {
+            if (lineToMove < 2)
+                lineToMove++;
+        }
+
+        if (SwipeController.swipeLeft)
+        {
+            if (lineToMove > 0)
+                lineToMove--;
+        }
+
+        if (SwipeController.swipeUp)
+        {
+            if(controller.isGrounded)
+            Jump();
+        }
+
+        if (controller.isGrounded && !isJumping)
+            anim.SetBool ("isRunning", true);
+            else
+            anim.SetBool ("isRunning", false);
+        
+        UnityEngine.Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if (lineToMove == 0)
+            targetPosition += UnityEngine.Vector3.left * lineDistance;
+        else if (lineToMove == 2)
+            targetPosition += UnityEngine.Vector3.right * lineDistance;
+
+        if (transform.position == targetPosition)
+            return;
+        UnityEngine.Vector3 diff = targetPosition - transform.position;
+        UnityEngine.Vector3 moveDir = diff.normalized * 25 * Time.deltaTime;
+        if (moveDir.sqrMagnitude < diff.sqrMagnitude)
+            controller.Move(moveDir);
+        else
+            controller.Move(diff);
+            
+
+
+
+    }
+
+   
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        dir.y += gravity * Time.fixedDeltaTime;
+        controller.Move(dir * Time.fixedDeltaTime);
+    }
+
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "obstacle")
+        {
+            losePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag == "coin")
+        {
+            coins++;
+            PlayerPrefs.SetInt("coins", coins);
+            coinsText.text = coins.ToString();
+            Destroy(other.gameObject);
+        }
+    }
+}
